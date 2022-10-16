5232a51b7217843260d3162d500bb7b23c2f277e
Pankaj Upreti <pankaj.upreti@hotmail.com>
2022-10-16 19:16:46 +0530

    first commit

---
+++ src/main/java/com/example/angular/springbootcrudapi/model/Employee.java
@@ -0,0 +1,122 @@
+package com.example.angular.springbootcrudapi.model;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Table
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    private Date hireDate;
+    private long salary;
+    private int managerId;
+    
+ 
+    private long phone;
+    private String department;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getHireDate() {
+		return hireDate;
+	}
+
+	public void setHireDate(Date hireDate) {
+		 
+		this.hireDate = hireDate;
+				//removeTime(hireDate);
+	}
+
+	public long getSalary() {
+		return salary;
+	}
+
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+
+	public int getManagerId() {
+		return managerId;
+	}
+
+	public void setManagerId(int managerId) {
+		this.managerId = managerId;
+	}
+	
+    public static Date removeTime(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return cal.getTime();
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+CREATE TABLE employees
+   ( employee_id NUMBER(6)
+   , first_name VARCHAR2(20)
+   , last_name VARCHAR2(25)   
+   , email VARCHAR2(25)
+   , phone_number VARCHAR2(20)
+   , hire_date DATE
+   , salary NUMBER(8,2)
+   , manager_id NUMBER(6)
+   , department_id NUMBER(4)   
+   ) ;
+   
+   
